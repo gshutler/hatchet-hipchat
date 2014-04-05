87f2a52d6c131d12f9594658eed485dc3c282c81
Garry Shutler <garry@robustsoftware.co.uk>
2014-04-05 22:04:25 +0100

    Limited backtrace from emoticon formatter

--- lib/hatchet-hipchat/hipchat_emoticon_formatter.rb
+++ lib/hatchet-hipchat/hipchat_emoticon_formatter.rb
@@ -14,16 +14,27 @@ module Hatchet
   # Emoticon reference: http://hipchat-emoticons.heroku.com/
   #
   class HipChatEmoticonFormatter
+    include BacktraceFormatter
 
     # Internal: Map for converting levels to emoticons.
     #
     LEVEL_EMOTICON_MAP = {
-      debug: '(content)',
-      info:  '(wat)',
-      warn:  '(ohcrap)',
-      error: '(omg)',
-      fatal: '(boom)'
+      :debug => '(content)',
+      :info  => '(wat)',
+      :warn  => '(ohcrap)',
+      :error => '(omg)',
+      :fatal => '(boom)'
     }
+
+    # Internal: The number of line to limit backtraces to by default.
+    #
+    DEFAULT_BACKTRACE_LIMIT = 4
+
+    # Public: Creates a new instance.
+    #
+    def initialize(args = {})
+      backtrace_limit = args.fetch(:backtrace_limit, DEFAULT_BACKTRACE_LIMIT)
+    end
 
     # Public: Returns the formatted message with HipChat emoticons in place of
     # the level.
@@ -46,11 +57,14 @@ module Hatchet
     #
     def format(level, context, message)
       msg = message.to_s.strip
+
       if message.ndc.any?
-        "#{LEVEL_EMOTICON_MAP[level]} - #{context} #{message.ndc.join(' ')} - #{msg}"
+        msg = "#{LEVEL_EMOTICON_MAP[level]} - #{context} #{message.ndc.join(' ')} - #{msg}"
       else
-        "#{LEVEL_EMOTICON_MAP[level]} - #{context} - #{msg}"
+        msg = "#{LEVEL_EMOTICON_MAP[level]} - #{context} - #{msg}"
       end
+
+      with_backtrace(message, msg)
     end
 
   end
--- lib/hatchet-hipchat/version.rb
+++ lib/hatchet-hipchat/version.rb
@@ -1,5 +1,5 @@
 module Hatchet
   module Hipchat
-    VERSION = '0.2.0'
+    VERSION = '0.3.0'
   end
 end
